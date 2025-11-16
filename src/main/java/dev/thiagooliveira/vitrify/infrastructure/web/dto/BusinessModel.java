0cab21bbb2fbcc6035b16aa3c07b3bdf56791af6
Thiago Oliveira <thiago@thiagoti.com>
2025-11-16 16:54:50 +0000

    add logo mock dynamic css

--- src/main/java/dev/thiagooliveira/vitrify/infrastructure/web/dto/BusinessModel.java
+++ src/main/java/dev/thiagooliveira/vitrify/infrastructure/web/dto/BusinessModel.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class BusinessModel {
-
+  private final String fontColor;
+  private final String headerColor;
+  private final String backgroundColor;
   private final String name;
   private final String address;
   private final List<SocialLinkModel> socialLinks;
@@ -13,6 +15,9 @@ public class BusinessModel {
   private final List<CatalogModel> catalogs;
 
   public BusinessModel(Business business) {
+    this.fontColor = "#ffffff";
+    this.headerColor = "#3a3f42";
+    this.backgroundColor = "#f8f9fa";
     this.name = business.getName();
     this.address = business.getAddress().orElse(null);
     this.socialLinks =
@@ -41,5 +46,17 @@ public class BusinessModel {
 
   public List<SocialLinkModel> getSocialLinks() {
     return socialLinks;
+  }
+
+  public String getFontColor() {
+    return fontColor;
+  }
+
+  public String getHeaderColor() {
+    return headerColor;
+  }
+
+  public String getBackgroundColor() {
+    return backgroundColor;
   }
 }
